8069ea2f16633fe52d8d0b278533570bc7687ce0
anasim <anasim@egain.com>
2024-09-26 16:49:26 +0530

    Tailwind configured for the app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-          Checking the first deployment
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center m-10 text-3xl font-bold underline">
+      <h2>Salaam!</h2>
     </div>
   );
 }
