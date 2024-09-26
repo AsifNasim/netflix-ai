7add42adf1c731bf6a0c2033bf0cad861fe50ce9
anasim <anasim@egain.com>
2024-09-26 23:50:22 +0530

    Added bg and logo of netflix.

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
          bg-cover absolute bg-center h-screen w-screen">
          </div>
          
-         <form className='w-full sm:w-1/2 md:w-1/2 sm:h-[60vh] lg:w-3/12 h-[100vh] p-8 sm:p-12 bg-black bg-opacity-80 my-20 sm:my-20 mx-auto absolute right-0 left-0 rounded-md'>
+         <form className='w-full sm:w-1/2 md:w-1/2 sm:h-[60vh] lg:w-3/12 h-[100vh] p-8 sm:p-12 bg-black bg-opacity-70 my-20 sm:my-20 mx-auto absolute right-0 left-0 rounded-md'>
     <label className='text-white block mx-2 my-4 text-lg sm:text-xl'>Sign In</label>
     <input type='text' placeholder='Email Address' className='px-3 py-2 m-2 bg-gray-700 border border-black rounded-md w-full' />
     <input type='password' placeholder='Password' className='px-3 py-2 bg-gray-700 border m-2 border-black rounded-md w-full'/>
