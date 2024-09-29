9968da95b9b0c1fb4ec6291a155862277bda3c0e
anasim <anasim@egain.com>
2024-09-30 00:11:18 +0530

    Form Validation has been added.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+  apiKey: "AIzaSyB7p8b2Z4Qzhf3mCaPOUkR83Sz4zrs0YPg",
+  authDomain: "zeeflix-ai.firebaseapp.com",
+  projectId: "zeeflix-ai",
+  storageBucket: "zeeflix-ai.appspot.com",
+  messagingSenderId: "44423812587",
+  appId: "1:44423812587:web:7d120706b69a0ab6c275ae",
+  measurementId: "G-RSCV5Y6VJ8"
 };
 
 // Initialize Firebase
