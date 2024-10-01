5b1fda7b2f7a9e52f4393d62f47fea059205f8f5
anasim <anasim@egain.com>
2024-10-02 00:12:17 +0530

    Firebase authentication fixed.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,25 +7,16 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// export const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDlenh5klMzVfqPfJpyPoYs61vSjWJ48n0",
-  authDomain: "zeeflixai.firebaseapp.com",
-  projectId: "zeeflixai",
-  storageBucket: "zeeflixai.appspot.com",
-  messagingSenderId: "614291446506",
-  appId: "1:614291446506:web:a538a63a532c91851f5593",
-  measurementId: "G-7HYL3QY1W9"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
