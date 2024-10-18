118b9e05c6337372eb4588ebd8e239e94ffd7beb
anasim <anasim@egain.com>
2024-10-18 11:24:15 +0530

    TMDB Movie has been added in a custom hook which is going to be used to display movies in main and secondary container.

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { API_OPTIONS, getMoviePlayNowURL } from "../utils/constants";
@@ -6,7 +7,6 @@ import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const useNowPlayingMovies = () => {
     const dispatch = useDispatch();
-
     useEffect( ()=>{
       getMoviePlayNowList();
     }, [])
@@ -15,7 +15,7 @@ const useNowPlayingMovies = () => {
         const data = await fetch(getMoviePlayNowURL,  API_OPTIONS);
         const result = await data.json();
         console.log("Now Movie response -->", result?.results);
-        dispatch(addNowPlayingMovies(result?.results))
+        dispatch(addNowPlayingMovies(result?.results));
   
   
     }
